2a007233dd3242273d0b6fb7cd932ba2cb55fda2
Lucas A <lucasalvine@hotmail.com>
2017-10-29 21:44:05 -0200

    algoritmo

---
+++ Algoritmo/src/mensagem/MensagemCoordenador.java
@@ -0,0 +1,14 @@
+
+package mensagem;
+
+/**
+ *
+ * @author Lucas Alvine
+ */
+public class MensagemCoordenador extends Mensagem {
+
+    public MensagemCoordenador(int idEmissor) {
+        super(idEmissor);
+    }
+    
+}
---
+++ Algoritmo/src/mensagem/MensagemEleicao.java
@@ -0,0 +1,14 @@
+
+package mensagem;
+
+/**
+ *
+ * @author Lucas Alvine
+ */
+public class MensagemEleicao extends Mensagem {
+
+    public MensagemEleicao(int idEmissor) {
+        super(idEmissor);
+    }
+    
+}
